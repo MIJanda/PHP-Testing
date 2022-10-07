3a0d6864bc9f5a6327023ed0242634797fe29a39
MIJanda <mokiliisaacjanda@gmail.com>
2022-10-07 14:18:29 +0300

    ExampleService object

--- 02_test_doubles/src/ExampleService.php
+++ 02_test_doubles/src/ExampleService.php
@@ -4,8 +4,22 @@ namespace App;
 
 class ExampleService 
 {
+    private $param1;
+    private $param2;
+
+    public function __construct($param1, $param2) {
+        $this->param1 = $param1;
+        $this->param2 = $param2;
+    }
+
+
     public function doSomething($arg)
     {
-        
+        return 'some random text';
+    }
+
+    public function nonMockedMethod($arg)
+    {
+        return $this->doSomething($arg);
     }
 }
